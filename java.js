34b12310ad8221ab3be8da2d28efa6dce79fb004
Baptiste Etroy <baptiste.etroy@gmail.com>
2024-09-13 18:29:47 +0200

    minor changes for a more smooth design

--- java.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('emailForm').addEventListener('submit', function(e) {
-    e.preventDefault();  // Prevent form submission for demo purposes
-
-    // Allow form submission
-    this.submit();
-});
