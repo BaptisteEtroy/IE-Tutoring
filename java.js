9806d72aa6a6f12c8f44925cac6aaa772903c897
Baptiste Etroy <baptiste.etroy@gmail.com>
2024-09-16 15:50:54 +0200

    modifications for a working slideshow, just need to modify the content of them

--- java.js
+++ java.js
@@ -1,17 +1,33 @@
 let slideIndex = 1;
-showSlides(slideIndex);function plusSlide(n) {
+showSlides(slideIndex);
+
+setInterval(() => {
+    nextSlide();
+  }, 3000); // 3 seconds
+
+function plusSlide(n) {
   showSlides(slideIndex += n);
-}function showSlides(n) {
+}
+
+function showSlides(n) {
   let i;
   const slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
-}function prevSlide() {
+  slides[slideIndex - 1].style.display = "block";
+}
+
+function prevSlide() {
   plusSlide(-1);
-}function nextSlide() {
+}
+
+function nextSlide() {
   plusSlide(1);
 }
