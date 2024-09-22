32cc76bcf7233601f385506f2b4eb20165e54402
Baptiste Etroy <baptiste.etroy@gmail.com>
2024-09-22 21:49:58 +0200

    Final changes for Version 2 of the website

--- java.js
+++ java.js
@@ -1,9 +1,9 @@
-let slideIndex = 1;
+let slideIndex = 0; // Start with the first slide
 showSlides(slideIndex);
 
 setInterval(() => {
     nextSlide();
-  }, 4000); // 3 seconds
+}, 3000); // 3 seconds
 
 function plusSlide(n) {
   showSlides(slideIndex += n);
@@ -12,16 +12,21 @@ function plusSlide(n) {
 function showSlides(n) {
   let i;
   const slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {
-    slideIndex = 1;
+  
+  if (n >= slides.length) {
+    slideIndex = 0; // Loop back to the first slide
   }
-  if (n < 1) {
-    slideIndex = slides.length;
+  if (n < 0) {
+    slideIndex = slides.length - 1; // Go to the last slide
   }
+  
+  // Hide all slides by removing the active class
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].classList.remove("active");
   }
-  slides[slideIndex - 1].style.display = "block";
+  
+  // Show the current slide by adding the active class
+  slides[slideIndex].classList.add("active");
 }
 
 function prevSlide() {
